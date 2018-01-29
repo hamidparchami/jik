19a0035000c36abdad84d88f836bf5830ccd7ed6
Hamid <hamidparchami@gmail.com>
2018-01-29 11:05:12 +0330

    change telegram config

--- config/telegram.php
+++ config/telegram.php
@@ -8,5 +8,5 @@
 return [
     'api_key'       => env('TELEGRAM_API_KEY', '539741359:AAG_D-BTWC2k1Ihg2rBr6X3y5v-EiTTirPo'),
     'username'      => env('TELEGRAM_USERNAME', 'HamidsFirstBot'),
-    'webhook_url'   => env('TELEGRAM_WEBHOOK_URL', 'https://snooopi.com/api/v1/request/reception'),
+    'webhook_url'   => env('TELEGRAM_WEBHOOK_URL', 'https://jikopeek.ir/api/v1/request/reception'),
 ];
